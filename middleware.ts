f1927c862ef9bb39088c5a10d932e5c937409344
ccorbett0116 <100178889+ccorbett0116@users.noreply.github.com>
2025-05-18 20:04:40 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,10 +3,10 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const hostname = request.headers.get('host') || ''
+  const hostname = request.headers.get('host')?.toLowerCase() || ''
   const url = request.nextUrl
 
-  if (hostname.startsWith('projects.')) {
+  if (hostname.startsWith('projects.') && !url.pathname.startsWith('/projects')) {
     url.pathname = `/projects${url.pathname}`
   }
 
