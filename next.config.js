f45b58066221a7fb3708aee2a9d7759d17a96534
root <root@www.colecorbett.ca>
2025-05-21 04:14:04 -0400

    changed config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,2 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  swcMinify: true,  // Uses Rust-based SWC for faster minification
-    compiler: {
-      styledComponents: true,  // If using CSS-in-JS
-  },
-}
+module.exports = {}
